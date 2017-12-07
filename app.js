1faf13219d6797363c2714b4e0a837f186607e8a
Dre <ldsz89@mail.missouri.edu>
2017-12-07 13:42:15 -0600

    Adding Firebase

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCsMmS7TtPL8BHRCQPvfgINmCk3b4KNdrc",
+  authDomain: "christmas-a14ca.firebaseapp.com",
+  databaseURL: "https://christmas-a14ca.firebaseio.com",
+  projectId: "christmas-a14ca",
+  storageBucket: "",
+  messagingSenderId: "212981809423"
+};
+firebase.initializeApp(config);
+
 var members = [
   {
     name: "Dre",
@@ -35,9 +46,7 @@ var app = new Vue({
   el: '#christmas',
   data: {
     members: (localStorage.getItem('members')!==null) ? JSON.parse(localStorage.getItem('members')) : members,
-    activeMember: {
-      name: 'Dre'
-    },
+    activeMember: this.members[0],
   },
   methods: {
     changeActiveMember: function (member) {
---
+++ firebase.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCsMmS7TtPL8BHRCQPvfgINmCk3b4KNdrc",
+  authDomain: "christmas-a14ca.firebaseapp.com",
+  databaseURL: "https://christmas-a14ca.firebaseio.com",
+  projectId: "christmas-a14ca",
+  storageBucket: "",
+  messagingSenderId: "212981809423"
+};
+firebase.initializeApp(config);
